923b5c7829fa099b8bf34ba43672ccc32dd72fc7
Olaoye Ademola <demmydd44@gmail.com>
2024-10-11 02:59:33 -0700

    basic annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - add
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Returns sum of two floating-point numbers.
+    """
+    return a + b
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - to string
+"""
+
+
+def to_str(: float) -> str:
+    """
+    Returns the string representation of a float
+    """
+    return str(n)
