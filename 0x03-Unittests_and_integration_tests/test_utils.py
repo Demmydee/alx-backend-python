c8525f660bb83245b2a54d9490919fdf0c558efe
Olaoye Ademola <demmydd44@gmail.com>
2024-11-08 06:46:16 +0000

    parmetize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -44,3 +44,13 @@ class TestAccessNestedMap(unittest.TestCase):
         ({}, ("a",), KeyError),
         ({"a": 1}, ("a", "b"), KeyError),
     ])
+    def test_access_nested_map_exception(
+            self,
+            nested_map: Mapping,
+            path: Sequence,
+            exception: Exception,
+            ) -> None:
+        '''Tests `access_nested_map`'s exception raising.
+        '''
+        with self.assertRaises(exception):
+            access_nested_map(nested_map, path)
